cbf15016b5fb81f403d41af5ebdaeb1cd94244fb
why <wanghuaiyu@highlight.mobi>
2023-05-10 00:05:52 +0800

    feat: update

--- lib_widget/src/main/java/com/xiaoyv/widget/kts/IntentKt.kt
+++ lib_widget/src/main/java/com/xiaoyv/widget/kts/IntentKt.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
 import androidx.core.os.bundleOf
+import com.blankj.utilcode.util.ToastUtils
 import com.blankj.utilcode.util.Utils
 import kotlin.reflect.KClass
 
@@ -12,10 +13,13 @@ import kotlin.reflect.KClass
  * IntentKt
  */
 fun <T> T.launchBy(launcher: ActivityResultLauncher<T>) {
-    launcher.launch(this)
+    runCatching {
+        launcher.launch(this)
+    }.onFailure {
+        ToastUtils.showShort(it.errorMsg)
+    }
 }
 
 @JvmOverloads
 fun <T : Context> KClass<T>.createIntent(bundle: Bundle = bundleOf()): Intent =
     Intent(Utils.getApp(), java).apply { putExtras(bundle) }
-
